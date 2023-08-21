4b9ebf7a3b06fc9fd49b137cce2336d1db537f31
Aashnaa <missaashnaa1@gmail.com>
2023-08-22 04:23:36 +0530

    execute commands functionality added

--- Space_craft_control.cpp
+++ Space_craft_control.cpp
@@ -17,28 +17,59 @@ public:
     this->direction = direction;
     this->angle = 0;
   }
+
+  void execute_commands(const std::vector<char> &commands)
+  {
+    for (char cmd : commands)
+    {
+      if (cmd == 'f')
+      {
+        move(1);
+      }
+      else if (cmd == 'b')
+      {
+        move(-1);
+      }
+      else if (cmd == 'l')
+      {
+        turn_left();
+      }
+      else if (cmd == 'r')
+      {
+        turn_right();
+      }
+      else if (cmd == 'u')
+      {
+        turn_up();
+      }
+      else if (cmd == 'd')
+      {
+        turn_down();
+      }
+    }
+  }
 };
 int main()
 {
+
+  string initial_direction = "";
+  cin >> initial_direction;
+  Spacecraft spacecraft(0, 0, 0, initial_direction);
+  vector<char> commands;
+  while (true)
   {
-    cout << "h";
-    string initial_direction = "N";
-    Spacecraft spacecraft(0, 0, 0, initial_direction);
-    vector<char> commands;
-    while (true)
-    {
-      char ch;
-      cin >> ch;
-      if (ch == '0')
-        break;
-      commands.push_back(ch);
-    }
-
-    spacecraft.execute_commands(commands);
-
-    cout << "Final Position: (" << spacecraft.getX() << ", " << spacecraft.getY() << ", " << spacecraft.getZ() << ")\n";
-    cout << "Final Direction: " << spacecraft.getDirection() << "\n";
-    cout << "Final Angle: " << spacecraft.getAngle() << " degrees\n";
-
-    return 0;
+    char ch;
+    cin >> ch;
+    if (ch == '0')
+      break;
+    commands.push_back(ch);
   }
+
+  spacecraft.execute_commands(commands);
+
+  cout << "Final Position: (" << spacecraft.getX() << ", " << spacecraft.getY() << ", " << spacecraft.getZ() << ")\n";
+  cout << "Final Direction: " << spacecraft.getDirection() << "\n";
+  cout << "Final Angle: " << spacecraft.getAngle() << " degrees\n";
+
+  return 0;
+}
